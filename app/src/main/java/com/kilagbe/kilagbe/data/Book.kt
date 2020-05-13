84e01378832c38b1adae7265594bd2e006afbe4f
kobi-2 <mueeze.nhpg@gmail.com>
2020-05-13 19:41:38 +0600

    added Book data class and Categories enum class

---
+++ app/src/main/java/com/kilagbe/kilagbe/data/Book.kt
@@ -0,0 +1,9 @@
+package com.kilagbe.kilagbe.data
+
+
+data class Book(val name: String, var price: Double) {
+    var authors = arrayListOf<String>()
+    var categories =  arrayListOf<Categories>()
+    lateinit var publisher: String
+    var amountInStock : Int? = null
+}
---
+++ app/src/main/java/com/kilagbe/kilagbe/data/Categories.kt
@@ -0,0 +1,5 @@
+package com.kilagbe.kilagbe.data
+
+enum class Categories {
+    Undergraduate, Postgraduate, EnglishMedium, NCTB, Abroad, Medical, Engineering, BBA, MBA, Literature
+}
