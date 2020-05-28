92f275c778085ec76f5543851fc6b824afaa9fb9
kobi-2 <mueeze.nhpg@gmail.com>
2020-05-28 20:01:25 +0600

    designed simple fragment for deliveryman all orders and my orders

---
+++ app/src/main/java/com/kilagbe/kilagbe/ui/deliveryman/DeliveryAllOrdersFragment.kt
@@ -0,0 +1,24 @@
+package com.kilagbe.kilagbe.ui.deliveryman
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.kilagbe.kilagbe.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DeliveryAllOrdersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_delivery_all_orders, container, false)
+    }
+
+}
