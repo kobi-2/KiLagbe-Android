055b1acb5dc2e654bcf6855b8d987f463a870bfa
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-06-17 23:54:01 +0600

    Refactor profile fragments a little bit more. The deliveryman profile fragments are fixed noe as well.

--- app/src/main/java/com/kilagbe/kilagbe/data/User.kt
+++ app/src/main/java/com/kilagbe/kilagbe/data/User.kt
@@ -1,6 +1,6 @@
 package com.kilagbe.kilagbe.data
 
-data class User(val name: String, val email: String, val phone: String) {
+data class User(var name: String, var email: String, val phone: String) {
     var uid: String? = null
     constructor():this("", "", "")
 }
