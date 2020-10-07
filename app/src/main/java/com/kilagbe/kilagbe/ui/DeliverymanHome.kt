0fd0ebf3cef6dc4ef13c36be5878b1ebe4151f2d
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-10-08 02:25:59 +0600

    Set up deliveryman homepage with sidebar.

--- app/src/main/java/com/kilagbe/kilagbe/ui/DeliverymanHome.kt
+++ app/src/main/java/com/kilagbe/kilagbe/ui/DeliverymanHome.kt
@@ -1,49 +1,33 @@
 package com.kilagbe.kilagbe.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.kilagbe.kilagbe.R
 
 class DeliverymanHome : AppCompatActivity() {
 
-
-    private lateinit var appBarConfiguration : AppBarConfiguration
-
+    lateinit var nav: NavigationView
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deliveryman_home)
+        nav = findViewById(R.id.nav_menu)
+        drawerLayout = findViewById(R.id.drawer)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        val navView: BottomNavigationView = findViewById(R.id.delivery_nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
 
-        val navController = findNavController(R.id.delivery_nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_profile,
-                R.id.navigation_all_orders,
-                R.id.navigation_my_orders
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-
-    }
-
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.delivery_nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        nav.setupWithNavController(navController)
     }
 
 }
