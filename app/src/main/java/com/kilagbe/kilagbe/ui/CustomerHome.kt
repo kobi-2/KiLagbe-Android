2c1e863c3843904d083e4ebd7c3fcbb4c7ae9331
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-10-08 02:45:21 +0600

    Add autocompletetextview for search with dummy data. TODO: Populate array with names of books.

--- app/src/main/java/com/kilagbe/kilagbe/ui/CustomerHome.kt
+++ app/src/main/java/com/kilagbe/kilagbe/ui/CustomerHome.kt
@@ -1,6 +1,8 @@
 package com.kilagbe.kilagbe.ui
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
@@ -14,6 +16,9 @@ class CustomerHome : AppCompatActivity() {
     lateinit var nav: NavigationView
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
+    lateinit var actv: AutoCompleteTextView
+
+    private var suggestions = arrayOf("Mueeze", "Ehsan", "Fayad", "Galib", "Galiba", "Ehtesham", "Mubin", "Farhan", "Maruf")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,10 @@ class CustomerHome : AppCompatActivity() {
         nav = findViewById(R.id.nav_menu)
         drawerLayout = findViewById(R.id.drawer)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        actv = findViewById(R.id.actv_customer)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, suggestions)
+        actv.threshold = 1
+        actv.setAdapter(adapter)
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
