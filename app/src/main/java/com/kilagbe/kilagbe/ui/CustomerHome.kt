7198c84db53f7e874d0a676bed70db5c86cf8b68
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2020-10-13 17:02:11 +0600

    Fetch list of books and populate for autocompletetextview

--- app/src/main/java/com/kilagbe/kilagbe/ui/CustomerHome.kt
+++ app/src/main/java/com/kilagbe/kilagbe/ui/CustomerHome.kt
@@ -1,6 +1,7 @@
 package com.kilagbe.kilagbe.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -10,27 +11,33 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.kilagbe.kilagbe.R
+import com.kilagbe.kilagbe.data.Book
+import com.kilagbe.kilagbe.databasing.ItemHelper
 
-class CustomerHome : AppCompatActivity() {
+class CustomerHome : AppCompatActivity(), ItemHelper.getAllBooksSuccessListener, ItemHelper.getAllBooksFailureListener {
 
     lateinit var nav: NavigationView
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
     lateinit var actv: AutoCompleteTextView
+    lateinit var ih: ItemHelper
 
-    private var suggestions = arrayOf("Mueeze", "Ehsan", "Fayad", "Galib", "Galiba", "Ehtesham", "Mubin", "Farhan", "Maruf")
+    private var suggestions = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_home)
 
+        ih = ItemHelper()
+        ih.setGetAllBooksSuccessListener(this)
+        ih.setGetAllBooksFailureListener(this)
+
+        ih.getAllBooks()
+
         nav = findViewById(R.id.nav_menu)
         drawerLayout = findViewById(R.id.drawer)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        actv = findViewById(R.id.actv_customer)
-        val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, suggestions)
-        actv.threshold = 1
-        actv.setAdapter(adapter)
+
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
@@ -38,5 +45,20 @@ class CustomerHome : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
 
         nav.setupWithNavController(navController)
+    }
+
+    override fun getAllBooksSuccess(bookArray: ArrayList<Book>) {
+        for ( book in bookArray )
+        {
+            suggestions.add(book.name)
+        }
+        actv = findViewById(R.id.actv_customer)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_item, suggestions)
+        actv.threshold = 1
+        actv.setAdapter(adapter)
+    }
+
+    override fun getAllBooksFailure() {
+        TODO("Not yet implemented")
     }
 }
